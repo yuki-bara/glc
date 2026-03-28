23587062f906314266b515aedd5455a756a4e5cb
yuki-bara <yukibara30@gmail.com>
2026-03-28 19:16:02 +0700

    add: update code part to 1.1

--- src/main.go
+++ src/main.go
@@ -10,6 +10,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"sync"
 )
 
 var version = "version"
@@ -21,38 +22,45 @@ type License struct {
 }
 
 func main() {
-	if len(os.Args) != 2 {
+	if len(os.Args) < 2 {
 		log.Fatal("Usage: lses <license-name>")
 	}
+	var wg sync.WaitGroup
+	for i := 1; i < len(os.Args); i++ {
+		wg.Add(1)
+		go func(licenseName string) {
+			defer wg.Done()
+			fmt.Printf("GLC-%s  %s\n", version, url)
 
-	fmt.Printf("GLC-%s  %s\n", version, url)
+			url := "https://api.github.com/licenses/" + licenseName
 
-	licenseName := os.Args[1]
-	url := "https://api.github.com/licenses/" + licenseName
+			resp, err := http.Get(url)
+			if err != nil {
+				log.Fatalf("Error fetching license: %v", err)
+			}
 
-	resp, err := http.Get(url)
-	if err != nil {
-		log.Fatalf("Error fetching license: %v", err)
+			if resp.StatusCode != http.StatusOK {
+				log.Fatalf("Error: License '%s' not found (HTTP %d)", licenseName, resp.StatusCode)
+			}
+			defer resp.Body.Close()
+			jsonData, _ := io.ReadAll(resp.Body)
+			var lic License
+			err = json.Unmarshal(jsonData, &lic)
+			if err != nil {
+				fmt.Printf("Error parsing JSON: %v\n", err)
+				return
+			}
+
+			// Create a file.
+			err = os.WriteFile(lic.Name, []byte(lic.Body), 0644)
+			if err != nil {
+				log.Fatalf("Error writing file: %v", err)
+				return
+			}
+
+			fmt.Printf("Successfully generated %s LICENSE file!\n", licenseName)
+		}(os.Args[i])
 	}
+	wg.Wait()
 
-	if resp.StatusCode != http.StatusOK {
-		log.Fatalf("Error: License '%s' not found (HTTP %d)", licenseName, resp.StatusCode)
-	}
-	defer resp.Body.Close()
-	jsonData, _ := io.ReadAll(resp.Body)
-	var lic License
-	err = json.Unmarshal(jsonData, &lic)
-	if err != nil {
-		fmt.Printf("Error parsing JSON: %v\n", err)
-		return
-	}
-
-	// Create a file.
-	err = os.WriteFile(lic.Name, []byte(lic.Body), 0644)
-	if err != nil {
-		log.Fatalf("Error writing file: %v", err)
-		return
-	}
-
-	fmt.Printf("Successfully generated %s LICENSE file!\n", licenseName)
 }
